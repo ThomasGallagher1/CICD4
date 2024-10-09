9940d1b302addda0f56049fc150faf63bfc3d050
G00415457@atu.ie <G00415457@atu.ie>
2024-10-09 15:17:17 +0100

    Added other values in Person class

--- src/main/java/atu/ie/week4cicd/Person.java
+++ src/main/java/atu/ie/week4cicd/Person.java
@@ -1,5 +1,6 @@
 package atu.ie.week4cicd;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
@@ -11,8 +12,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class Person {
-    @NotBlank(message =" you must enter a name")
+    @NotBlank(message ="You must enter a name")
     private String name;
     @Min(value=1, message="Must be greater or equal to 1")
     private int id;
+    @NotBlank(message ="You must enter a title")
+    private String title;
+    @Min(value=0, message="Must be greater or equal to 0")
+    private int age;
+    @Email(message ="You must enter a valid email")
+    private String email;
+    @NotBlank(message ="You must enter a position")
+    private String position;
+    @NotBlank(message ="You must enter a department")
+    private String department;
 }
